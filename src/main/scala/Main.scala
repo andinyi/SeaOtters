741b3b3a463f84652c22f025ab2bf792e60dd29a
Fenix3123 <63925178+Fenix3123@users.noreply.github.com>
2022-07-12 16:02:36 -0500

    update

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,8 @@
 object Main {
   def main(args: Array[String]): Unit = {
-
     val session = new SparkInit("Project Sea Otters")
-    session.spark.read.csv("datasets/covid_19_data.csv").show()
+    //session.spark.read.option("header", "true").csv("datasets/covid_19_data.csv").show()
+    session.spark.read.option("header", "true").csv("datasets/owid-covid-data.csv").show()
   }
 }
 
