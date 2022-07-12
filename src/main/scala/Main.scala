9993ee5099ed5f29137221bb5f2e822bacc4122c
Andy <hni8449@bths.edu>
2022-07-12 16:09:15 -0400

    formating

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,5 @@
 object Main {
   def main(args: Array[String]): Unit = {
-
     val session = new SparkInit("Project Sea Otters")
     session.spark.read.csv("datasets/covid_19_data.csv").show()
   }
