d2a755f3c4be3195c77214f29789b1f66a328d64
Andy <hni8449@bths.edu>
2022-07-12 15:55:42 -0400

    added initial

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 object Main {
   def main(args: Array[String]): Unit = {
     val file = new importCsv
-    val session = new SparkInit
+    val session = new SparkInit("Project Sea Otters")
   }
+}
 
