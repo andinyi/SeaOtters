4b7e3af4e093118ca41c47f8948e82ad5233951f
Alex <aalleexxdss@gmail.com>
2022-07-13 12:12:57 -0600

    query2 added

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,16 @@
+import org.apache.spark
+import org.apache.spark.sql
+
 object Main {
   def main(args: Array[String]): Unit = {
     val session = new SparkInit("Project Sea Otters")
-    session.spark.read.csv("datasets/covid_19_data.csv").show()
+    //session.spark.read.csv("datasets/covid_19_data.csv").show()
+
+    val dataDf = session.spark.read.format("csv").option("header","true").load("datasets/owid-covid-data.csv")
+    dataDf.createOrReplaceTempView("dataView")
+
+    session.spark.sql(Q2.query2).show(false)
+
   }
 }
 
--- src/main/scala/SparkInit.scala
+++ src/main/scala/SparkInit.scala
@@ -2,13 +2,13 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
 class SparkInit (appName:String){
+
  val spark = SparkSession
    .builder
    .appName(appName)
    .config("spark.master", "local[*]")
    .enableHiveSupport()
    .getOrCreate()
-
   Logger.getLogger("org").setLevel(Level.OFF)
   Logger.getLogger("aka").setLevel(Level.OFF)
 }
