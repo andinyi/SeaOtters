4c9d80d0eceba1eaf6515da88ba6d38b1156e211
Andy <hni8449@bths.edu>
2022-07-13 11:26:48 -0400

    query1

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,17 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.DecimalType
+import org.apache.spark.sql.{AnalysisException, DataFrame, Dataset, SaveMode, SparkSession}
+
 object Main {
   def main(args: Array[String]): Unit = {
     val session = new SparkInit("Project Sea Otters")
-    val df = session.spark.read.option("header", "true").csv("datasets/owid-covid-data.csv").show()
+    var df = session.spark.read.option("header", "true").csv("datasets/owid-covid-data.csv")
+    df = df.withColumn("total_cases", col("total_cases").cast(DecimalType(18, 1)))
+    df = df.withColumn("total_deaths", col("total_deaths").cast(DecimalType(18, 1)))
+    df.createOrReplaceTempView("query1")
+    session.spark.sql(Query1.query1).show(false)
   }
 }
 
