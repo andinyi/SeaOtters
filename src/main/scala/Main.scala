e3dc20bfcdd6967a4257973705dfa621364509fd
Andy <hni8449@bths.edu>
2022-07-13 10:31:00 -0400

    df function

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,7 @@
 object Main {
   def main(args: Array[String]): Unit = {
     val session = new SparkInit("Project Sea Otters")
-    session.spark.read.csv("datasets/covid_19_data.csv").show()
+    val df = session.spark.read.option("header", "true").csv("datasets/owid-covid-data.csv").show()
   }
 }
 
