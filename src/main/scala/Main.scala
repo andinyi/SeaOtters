9275040143ac4a5bc8ddaa5cc6c8569632ccb0bc
Fenix3123 <63925178+Fenix3123@users.noreply.github.com>
2022-07-13 14:36:29 -0500

    Query 7

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,8 +8,9 @@ object Main {
     val df1 = session.spark.sql("Select * from owid_tb").toDF()
     df1.show()
     import session.spark.implicits._
-    //val q1 = df1.select($"total_deaths".cast(IntegerType), $"population".cast(IntegerType), $"location", $"date").toDF()
-    val q1 = df1.toDF()
+    val q1 = df1.select($"people_vaccinated".cast(IntegerType), $"population".cast(IntegerType), $"location", $"date").toDF()
+    q1.createOrReplaceTempView("owid")
+    session.spark.sql("Select location, cast((people_vaccinated/population)*100 as decimal(8,5)) As Vaccinated from owid where date='2022-07-10' order by Vaccinated desc").show()
 
 
 
