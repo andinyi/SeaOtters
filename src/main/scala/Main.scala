bf04fa15905b527bd9a89d0d35b1c9a2c3aef2ba
Fenix3123 <63925178+Fenix3123@users.noreply.github.com>
2022-07-14 14:20:43 -0500

    Created query 10

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,16 +8,17 @@ import org.apache.spark.sql
 object Main {
   def main(args: Array[String]): Unit = {
     val session = new SparkInit("Project Sea Otters")
-    var df = session.spark.read.option("header", "true").csv("datasets/owid-covid-data.csv")
-    df = df.withColumn("total_cases", col("total_cases").cast(DecimalType(18, 1)))
-    df = df.withColumn("total_deaths", col("total_deaths").cast(DecimalType(18, 1)))
-    df = df.withColumn("new_cases", col("new_cases").cast(DecimalType(18, 1)))
-    df = df.withColumn("population", col("population").cast(DecimalType(18, 1)))
-    df.createOrReplaceTempView("data") //query 1
+    //var df = session.spark.read.option("header", "true").csv("datasets/owid-covid-data.csv")
+    //df.createOrReplaceTempView("data")
+
+    session.spark.sql("Select location, median_age,Round(((total_cases - total_deaths)/population)*100, 4) as recovered from owid_tb where date = '2022-07-10'").show()
+
+
+    //query 1
     //session.spark.sql(Query1.query1).show(false)
     //df.createOrReplaceTempView("dataView") //query 2
     //session.spark.sql(Q2.query2).show(false)
-    session.spark.sql(Query3.query3).show()
+    //session.spark.sql(Query3.query3).show()
     //session.spark.sql("SELECT location, MAX(new_cases) FROM data GROUP BY location").show()
 
     //df.createOrReplaceTempView("owid") //query 4
