d6688abda17ecf50700376d3f926f80d57f9ddc5
Andy <32392170+andinyi@users.noreply.github.com>
2022-07-14 15:29:10 -0400

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,12 +8,12 @@ import org.apache.spark.sql
 object Main {
   def main(args: Array[String]): Unit = {
     val session = new SparkInit("Project Sea Otters")
-    //var df = session.spark.read.option("header", "true").csv("datasets/owid-covid-data.csv")
-    //df.createOrReplaceTempView("data")
-
-    session.spark.sql("Select location, median_age,Round(((total_cases - total_deaths)/population)*100, 4) as recovered from owid_tb where date = '2022-07-10'").show()
-
-
+    var df = session.spark.read.option("header", "true").csv("datasets/owid-covid-data.csv")
+    df = df.withColumn("total_cases", col("total_cases").cast(DecimalType(18, 1)))
+    df = df.withColumn("total_deaths", col("total_deaths").cast(DecimalType(18, 1)))
+    df = df.withColumn("new_cases", col("new_cases").cast(DecimalType(18, 1)))
+    df = df.withColumn("population", col("population").cast(DecimalType(18, 1)))
+    df.createOrReplaceTempView("data")
     //query 1
     //session.spark.sql(Query1.query1).show(false)
     //df.createOrReplaceTempView("dataView") //query 2
