853a483716195aa4b5ea2cf099a967d1fef39b11
Alex <aalleexxdss@gmail.com>
2022-07-21 12:31:28 -0600

    multithreading added

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,6 +9,19 @@ import org.apache.spark.sql
 
 import scala.io.StdIn.readLine
 
+
+class QueryThread(val query: String, val session: SparkInit) extends Thread {
+  override def run(): Unit = {
+    session.spark.sql(query).show(false)
+  }
+}
+
+class OutputThread(val query: String, val session: SparkInit, val outputLocation: String) extends Thread {
+  override def run(): Unit = {
+    session.spark.sql(query).coalesce(1).write.mode(SaveMode.Overwrite).option("header", "true").csv(outputLocation)
+  }
+}
+
 object Main {
   def main(args: Array[String]): Unit = {
     var method = "default"
@@ -54,6 +67,8 @@ object Main {
 
     if(in == "1") {
       //queries
+
+      /*
       session.spark.sql(queries.query1()).show(false)
       session.spark.sql(queries.query2()).show(false)
       session.spark.sql(queries.query3()).show(false)
@@ -64,9 +79,68 @@ object Main {
       session.spark.sql(queries.query8()).show(false)
       session.spark.sql(queries.query9()).show(false)
       session.spark.sql(queries.query10()).show(false)
+
+       */
+
+      println("Starting query 1")
+      val Qthread1 = new Thread(new QueryThread(queries.query1(), session))
+      Qthread1.start()
+
+      println("Starting query 2")
+      val Qthread2 = new Thread(new QueryThread(queries.query2(), session))
+      Qthread2.start()
+
+      println("Starting query 3")
+      val Qthread3 = new Thread(new QueryThread(queries.query3(), session))
+      Qthread3.start()
+
+      println("Starting query 4")
+      val Qthread4 = new Thread(new QueryThread(queries.query4(), session))
+      Qthread4.start()
+
+      println("Starting query 5")
+      val Qthread5 = new Thread(new QueryThread(queries.query5(), session))
+      Qthread5.start()
+
+      println("Starting query 6")
+      val Qthread6 = new Thread(new QueryThread(queries.query6(), session))
+      Qthread6.start()
+
+      println("Starting query 7")
+      val Qthread7 = new Thread(new QueryThread(queries.query7(), session))
+      Qthread7.start()
+
+      println("Starting query 8")
+      val Qthread8 = new Thread(new QueryThread(queries.query8(), session))
+      Qthread8.start()
+
+      println("Starting query 9")
+      val Qthread9 = new Thread(new QueryThread(queries.query9(), session))
+      Qthread9.start()
+
+      println("Starting query 10")
+      val Qthread10 = new Thread(new QueryThread(queries.query10(), session))
+      Qthread10.start()
+
+      Qthread1.join()
+      Qthread2.join()
+      Qthread3.join()
+      Qthread4.join()
+      Qthread5.join()
+      Qthread6.join()
+      Qthread7.join()
+      Qthread8.join()
+      Qthread9.join()
+      Qthread10.join()
+
+
+
+
     }
     if(in == "2") {
       //queries
+
+      /*
       session.spark.sql(queries.query1()).coalesce(1).write.mode(SaveMode.Overwrite).option("header", "true").csv("./resultCsv/query1/")
       session.spark.sql(queries.query2()).coalesce(1).write.mode(SaveMode.Overwrite).option("header", "true").csv("./resultCsv/query2/")
       session.spark.sql(queries.query3()).coalesce(1).write.mode(SaveMode.Overwrite).option("header", "true").csv("./resultCsv/query3/")
@@ -77,8 +151,64 @@ object Main {
       session.spark.sql(queries.query8()).coalesce(1).write.mode(SaveMode.Overwrite).option("header", "true").csv("./resultCsv/query8/")
       session.spark.sql(queries.query9()).coalesce(1).write.mode(SaveMode.Overwrite).option("header", "true").csv("./resultCsv/query9/")
       session.spark.sql(queries.query10()).coalesce(1).write.mode(SaveMode.Overwrite).option("header", "true").csv("./resultCsv/query10/")
+
+       */
+
+      println("Outputting query 1")
+      val outThread1 = new Thread(new OutputThread(queries.query1(), session, "./resultCsv/query1/"))
+      outThread1.start()
+
+      println("Outputting query 2")
+      val outThread2 = new Thread(new OutputThread(queries.query2(), session, "./resultCsv/query2/"))
+      outThread2.start()
+
+      println("Outputting query 3")
+      val outThread3 = new Thread(new OutputThread(queries.query3(), session, "./resultCsv/query3/"))
+      outThread3.start()
+
+      println("Outputting query 4")
+      val outThread4 = new Thread(new OutputThread(queries.query4(), session, "./resultCsv/query4/"))
+      outThread4.start()
+
+      println("Outputting query 5")
+      val outThread5 = new Thread(new OutputThread(queries.query5(), session, "./resultCsv/query5/"))
+      outThread5.start()
+
+      println("Outputting query 6")
+      val outThread6 = new Thread(new OutputThread(queries.query6(), session, "./resultCsv/query6/"))
+      outThread6.start()
+
+      println("Outputting query 7")
+      val outThread7 = new Thread(new OutputThread(queries.query7(), session, "./resultCsv/query7/"))
+      outThread7.start()
+
+      println("Outputting query 8")
+      val outThread8 = new Thread(new OutputThread(queries.query8(), session, "./resultCsv/query8/"))
+      outThread8.start()
+
+      println("Outputting query 9")
+      val outThread9 = new Thread(new OutputThread(queries.query9(), session, "./resultCsv/query9/"))
+      outThread9.start()
+
+      println("Outputting query 10")
+      val outThread10 = new Thread(new OutputThread(queries.query10(), session, "./resultCsv/query10/"))
+      outThread10.start()
+
+      outThread1.join()
+      outThread2.join()
+      outThread3.join()
+      outThread4.join()
+      outThread5.join()
+      outThread6.join()
+      outThread7.join()
+      outThread8.join()
+      outThread9.join()
+      outThread10.join()
+
     }
 
     session.logger.warn(s"$GREEN has finished running! Thanks for your time!$RESET")
   }
+
+
 }
