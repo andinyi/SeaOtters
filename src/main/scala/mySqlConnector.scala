c54020b6730cc0fa1c8499457dfb83442c17907d
Fenix3123 <63925178+Fenix3123@users.noreply.github.com>
2022-07-25 09:36:54 -0500

    ddsd
    dsds

--- src/main/scala/mySqlConnector.scala
+++ src/main/scala/mySqlConnector.scala
@@ -3,12 +3,12 @@ import org.apache.spark.sql.DataFrame
 class mySqlConnector {
   def run(session: SparkInit): DataFrame = {
     val driver = "com.mysql.cj.jdbc.Driver"
-    val username = "InsertUSER"
-    val password = "InsertPSW"
+    val username = "root"
+    val password = "Setiawan112!"
     Class.forName(driver)
     val df2 = session.spark.read.format("jdbc")
       .option("driver", "com.mysql.cj.jdbc.Driver")
-      .option("dbtable", "`owid-covid-data`")
+      .option("dbtable", "`owid_covid_data`")
       .option("url", "jdbc:mysql://localhost:3306/project2")
       .option("user", username)
       .option("password", password)
